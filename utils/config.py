4819d548d4654129bdbd59b8569dfaa7474fde23
jackonedev <jackone.action.software@gmail.com>
2024-08-20 16:50:20 -0300

    updated utils.config file

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,13 @@
 import os
 
+# Configuración de variables de entorno
+# para hacer requests al exchange BingX
+URL = "https://open-api.bingx.com"
+KLINES_SERVICE = URL + "/openApi/swap/v2/quote/klines"
+KLINES_LIMIT = 555
+
+
+# Configuración de directorios del proyecto
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATASETS_PATH = os.path.join(PROJECT_ROOT, "datasets")
 
