fffc0a6e8198ccd64601afb2cd7652c36b3d6133
jackonedev <jackone.action.software@gmail.com>
2024-08-20 18:55:58 -0300

    added schemas/temporality pydantic validation schema

---
+++ schemas/temporality.py
@@ -0,0 +1,56 @@
+import datetime as dt
+from typing import Optional
+
+from pydantic import BaseModel, Field, validator
+
+from tools.dates import past_timestamp
+
+
+class TempMappingModel(BaseModel):
+    _now = dt.datetime.now()
+
+    # Definición de la "t" temporalidades
+    t_1w: Optional[int] = Field(
+        default_factory=lambda: past_timestamp(
+            400, "days", TempMappingModel._now.default
+        )
+    )
+    t_1d: Optional[int] = Field(
+        default_factory=lambda: past_timestamp(
+            180, "days", TempMappingModel._now.default
+        )
+    )
+    t_4h: Optional[int] = Field(
+        default_factory=lambda: past_timestamp(
+            30, "days", TempMappingModel._now.default
+        )
+    )
+    t_1h: Optional[int] = Field(
+        default_factory=lambda: past_timestamp(8, "days", TempMappingModel._now.default)
+    )
+    t_15m: Optional[int] = Field(
+        default_factory=lambda: past_timestamp(
+            36, "hours", TempMappingModel._now.default
+        )
+    )
+    t_5m: Optional[int] = Field(
+        default_factory=lambda: past_timestamp(
+            12, "hours", TempMappingModel._now.default
+        )
+    )
+
+    @validator("*", pre=True)
+    def validate_timestamps(cls, v):
+        if v is not None and not isinstance(v, int):
+            raise ValueError("El valor debe ser un entero.")
+        return v
+
+    def to_dict(self):
+        return {
+            "1w": self.t_1w,
+            "1d": self.t_1d,
+            "4h": self.t_4h,
+            "1h": self.t_1h,
+            "15m": self.t_15m,
+            "5m": self.t_5m,
+        }
