cf1641ef65c719240b29dcc50a2d667bddd57af8
jackonedev <jackone.action.software@gmail.com>
2024-08-21 19:10:24 -0300

    added function to extract the latest download

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import itertools
 import os
 import random
 
@@ -24,3 +25,35 @@ def create_download_folders(data):
             final_folders.append(folder_name)
 
     return final_folders
+
+
+def obtain_most_recent_download_name():
+    "Devuelve el nombre del directorio de desarga más reciente en DATASETS_PATH"
+    lista = os.listdir(DATASETS_PATH)
+    lista_ordenada = sorted(
+        lista,
+        key=lambda x: (
+            dt.datetime.strptime(x.split("_")[0], "%Y-%m-%d"),
+            int(x.split("_")[1]),
+        ),
+        reverse=True,
+    )
+    return lista_ordenada[0]
+
+
+def obtain_download_files_structure() -> dict:
+    "Devuelve la lista de nombres de los archivos de descarga"
+    DATASETS_NAME = obtain_most_recent_download_name()
+    DATASETS_DIR = os.path.join(DATASETS_PATH, DATASETS_NAME)
+    CURRENCY_LIST = os.listdir(DATASETS_DIR)
+    TEMPORALITY_LIST = os.listdir(os.path.join(DATASETS_DIR, CURRENCY_LIST[0]))
+
+    structure = {}
+    for currency in CURRENCY_LIST:
+        structure[currency] = {}
+        for temporalidad in TEMPORALITY_LIST:
+            structure[currency][temporalidad] = os.path.join(
+                DATASETS_DIR, currency, temporalidad, "data.parquet"
+            )
+
+    return structure
