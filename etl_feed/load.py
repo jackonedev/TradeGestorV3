c830a5cdb21cbbe87caa0d6bc0b1de7dc01ac327
jackonedev <jackone.action.software@gmail.com>
2024-08-22 10:25:51 -0300

    updated etl_feed/load: includes moving average plots

--- etl_feed/load.py
+++ etl_feed/load.py
@@ -3,7 +3,7 @@ from typing import Dict
 import pandas as pd
 
 from tools.plots import (
-    create_band,
+    create_scatter,
     create_bar_figure,
     create_candlestick,
     create_price_figure,
@@ -32,22 +32,54 @@ def load(results: Dict[str, Dict[str, pd.DataFrame]], plots: bool = True) -> Non
             if not plots:
                 continue
             candlestick = create_candlestick(results[activo][temporalidad])
-            upper_band = create_band(
+            upper_band = create_scatter(
                 results[activo][temporalidad], "upper_BB", "upper_BB", "red"
             )
-            lower_band = create_band(
+            lower_band = create_scatter(
                 results[activo][temporalidad], "lower_BB", "lower_BB", "red"
             )
-            upper_KC = create_band(
+            upper_KC = create_scatter(
                 results[activo][temporalidad], "upper_KC", "upper_KC", "blue"
             )
-            lower_KC = create_band(
+            lower_KC = create_scatter(
                 results[activo][temporalidad], "lower_KC", "lower_KC", "blue"
             )
+            WMA_12 = create_scatter(
+                results[activo][temporalidad], "WMA_12", "WMA_12", "yellow"
+            )
+            DEMA_12 = create_scatter(
+                results[activo][temporalidad], "DEMA_12", "DEMA_12", "yellow"
+            )
+            TEMA_12 = create_scatter(
+                results[activo][temporalidad],
+                "TEMA_12",
+                "TEMA_12",
+                "yellow",
+                hover=True,
+            )
+            EMA_55 = create_scatter(
+                results[activo][temporalidad], "EMA_55", "EMA_55", "brown", hover=True
+            )
+            TRIMA_L_55 = create_scatter(
+                results[activo][temporalidad], "TRIMA_L_55", "TRIMA_L_55", "brown"
+            )
+            objects1 = [
+                candlestick,
+                upper_band,
+                lower_band,
+                upper_KC,
+                lower_KC,
+                WMA_12,
+                DEMA_12,
+                TEMA_12,
+                EMA_55,
+                TRIMA_L_55,
+            ]
+
             bar = create_SQZMOM_bar(results[activo][temporalidad])
             fig1 = create_price_figure(
                 data=results[activo][temporalidad],
-                graph_objects=[candlestick, upper_band, lower_band, upper_KC, lower_KC],
+                graph_objects=objects1,
                 title=f"{activo} {temporalidad}",
             )
             fig2 = create_bar_figure(results[activo][temporalidad], [bar])
