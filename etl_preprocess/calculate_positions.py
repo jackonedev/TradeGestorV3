27cc4c58dfa1b00002734db342fde2dd4040a5cb
jackonedev <jackone.action.software@gmail.com>
2024-08-25 20:51:20 -0300

    updated etl_preprocessing

--- etl_preprocess/calculate_positions.py
+++ etl_preprocess/calculate_positions.py
@@ -3,17 +3,8 @@ from typing import Dict, List, Tuple
 import numpy as np
 import pandas as pd
 
+from tg.trade_gestor_v1 import cargar_contrato, get_price
 from tools.business import calculate_targets, leverage, liq_price, split_targets
-from utils.utils import (
-    obtain_most_recent_download_directory_paths,
-    obtain_most_recent_downloaded_datasets,
-)
-
-download_paths = obtain_most_recent_download_directory_paths()
-downloaded_dfs = obtain_most_recent_downloaded_datasets()
-
-# obtener una muestra
-df = downloaded_dfs["BTC"]["5m"].copy()
 
 
 def return_targets(
@@ -70,11 +61,18 @@ def return_targets(
     targets = {}
     for activo, temporalidad_dict in df_asset_dict.items():
         targets[activo] = {}
-        # Obtener real-price: if "live"
-        # TODO - if live: real_price = get_real_price(activo)
-        # Es importante porque la data de analisis tiene delay
+        if live:
+            contract = cargar_contrato(activo)
+            real_price = get_price(contract["symbol"])
+            price_precision = contract["pricePrecision"]
+        else:
+            real_price = None
+            price_precision = 2
+
         for temporalidad, df in temporalidad_dict.items():
-            targets[activo][temporalidad] = calculate_targets(df, real_price=None)
+            targets[activo][temporalidad] = calculate_targets(
+                df, real_price, price_precision
+            )
     return targets
 
 
@@ -84,9 +82,12 @@ def return_positions(
     operation_volume: float = 100.0,
     market: bool = True,
     limit: bool = True,
-) -> None:
+    live: bool = False,
+) -> Dict[str, Dict[str, Dict]]:
+
     if direction.lower() not in ["long", "short"]:
-        raise ValueError
+        raise ValueError("Direction must be 'long' or 'short'")
+
     direction = direction.lower()
     positions = {}
     activo_list = list(targets.keys())
@@ -94,7 +95,14 @@ def return_positions(
     for activo, temporalidad_dict in targets.items():
         positions[activo] = {}
         operation_volume_activo = operation_volume / len(activo_list)
-        qty_precision: int = 5  # TODO: descargar contratos
+        if live:
+            contract = cargar_contrato(activo)
+            qty_precision = contract["quantityPrecision"]
+            price_precision = contract["pricePrecision"]
+        else:
+            qty_precision: int = 5
+            price_precision: int = 2
+
         for temporalidad, target_dict in temporalidad_dict.items():
             operation_volume_temporalidad = operation_volume_activo / len(
                 temporalidad_list
@@ -160,11 +168,74 @@ def return_positions(
                 "mean_stoploss_pct": mean_sl_pct,
                 "mean_takeprofit_pct": mean_tp_pct,
                 "lev": lev,
-                "liq": liq,
+                "liq": round(liq, price_precision),
                 "qty_per_entry": qty_per_entry,
-                "vol_per_entry": vol_unidad,
-                "vol_trade": operation_volume_temporalidad,
+                "vol_per_entry": round(vol_unidad, price_precision),
+                "vol_trade": round(operation_volume_temporalidad, price_precision),
                 "RB": rb,
             }
 
     return positions
+
+
+def adjust_positions_leverage(
+    positions: Dict[str, Dict[str, Dict]], direction: str, live: bool = False
+) -> Dict[str, List[Dict]]:
+    """
+    Adjusts the leverage of all positions to the minimum leverage in the asset list.
+    """
+
+    adj_positions_by_asset = {}
+    for asset in positions:
+        operations = list(positions[asset].values())
+        if live:
+            contract = cargar_contrato(asset)
+            max_leverage_l = int(contract["maxLongLeverage"])
+            max_leverage_s = int(contract["maxShortLeverage"])
+            qty_precision = contract["quantityPrecision"]
+        else:
+            max_leverage_l = 100
+            max_leverage_s = 100
+            qty_precision = 5
+
+        if direction == "long":
+            max_leverage = max_leverage_l
+        elif direction == "short":
+            max_leverage = max_leverage_s
+
+        min_lev = min(pos["lev"] for pos in operations)
+        min_liq = min(pos["liq"] for pos in operations)
+
+        if min_lev >= max_leverage:
+            min_lev = max_leverage
+            min_liq = "Not calculated"
+
+        positions_adj = []
+        for pos in operations:
+            lev_actual = pos["lev"]
+
+            if lev_actual == min_lev:
+                positions_adj.append(pos)
+                continue
+
+            adj_factor = lev_actual / min_lev
+
+            qty_per_entry_ajustada = np.array(
+                [qty * adj_factor for qty in pos["qty_per_entry"]]
+            )
+            vol_per_entry_ajustada = pos["vol_per_entry"] * adj_factor
+            vol_trade = pos["vol_trade"] * adj_factor
+
+            pos_adj = pos.copy()
+            pos_adj["lev"] = min_lev
+            pos_adj["liq"] = min_liq
+            pos_adj["qty_per_entry"] = qty_per_entry_ajustada.round(
+                qty_precision
+            ).tolist()
+            pos_adj["vol_per_entry"] = vol_per_entry_ajustada
+            pos_adj["vol_trade"] = vol_trade
+
+            positions_adj.append(pos_adj)
+        adj_positions_by_asset[asset] = positions_adj
+
+    return adj_positions_by_asset
