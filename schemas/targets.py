feaf798f9ac8cc1d053bd99a4267fb8bc7a9c653
jackonedev <jackone.action.software@gmail.com>
2024-08-25 12:41:32 -0300

    added precision for round price targets

--- schemas/targets.py
+++ schemas/targets.py
@@ -1,5 +1,4 @@
 from typing import Dict, List, Optional, Tuple
-
 from pydantic import BaseModel
 
 
@@ -18,82 +17,86 @@ class TargetResults(BaseModel):
     limit: Dict[str, List[Tuple[float, ...]]]
 
     @classmethod
-    def calculate(cls, df, real_price: Optional[float] = None):
+    def calculate(cls, df, real_price: Optional[float] = None, precision: int = 2):
         atr = df["atr"].iloc[-1]
         close_price = df["close"].iloc[-1]
         params = TradeParams(atr=atr, real_price=real_price or close_price)
 
         return cls(
             market={
-                "long": cls._calculate_market_long(params),
-                "short": cls._calculate_market_short(params),
+                "long": cls._calculate_market_long(params, precision),
+                "short": cls._calculate_market_short(params, precision),
             },
             limit={
-                "long": cls._calculate_limit_long(params),
-                "short": cls._calculate_limit_short(params),
+                "long": cls._calculate_limit_long(params, precision),
+                "short": cls._calculate_limit_short(params, precision),
             },
         )
 
     @staticmethod
-    def _calculate_market_long(params: TradeParams) -> List[Tuple[float, ...]]:
+    def _calculate_market_long(params: TradeParams, precision: int) -> List[Tuple[float, ...]]:
         return [
-            (params.real_price - 0.9 * params.atr,),  # Stop-loss
-            (params.real_price - params.market_spread,),  # Entry
+            (round(params.real_price - 0.9 * params.atr, precision),),  # Stop-loss
+            (round(params.real_price - params.market_spread, precision),),  # Entry
             (
-                params.real_price + 0.3 * params.atr,
-                params.real_price + params.atr,
+                round(params.real_price + 0.3 * params.atr, precision),
+                round(params.real_price + params.atr, precision),
             ),  # Take-profit
         ]
 
     @staticmethod
-    def _calculate_market_short(params: TradeParams) -> List[Tuple[float, ...]]:
+    def _calculate_market_short(params: TradeParams, precision: int) -> List[Tuple[float, ...]]:
         return [
-            (params.real_price + 0.9 * params.atr,),  # Stop-loss
-            (params.real_price + params.market_spread,),  # Entry
+            (round(params.real_price + 0.9 * params.atr, precision),),  # Stop-loss
+            (round(params.real_price + params.market_spread, precision),),  # Entry
             (
-                params.real_price - 0.3 * params.atr,
-                params.real_price - params.atr,
+                round(params.real_price - 0.3 * params.atr, precision),
+                round(params.real_price - params.atr, precision),
             ),  # Take-profit
         ]
 
     @staticmethod
-    def _calculate_limit_long(params: TradeParams) -> List[Tuple[float, ...]]:
+    def _calculate_limit_long(params: TradeParams, precision: int) -> List[Tuple[float, ...]]:
+        entry_1 = params.real_price - 0.25 * params.atr
+        entry_2 = params.real_price - params.atr
         return [
             (
-                params.real_price - 2.5 * params.atr,
-                params.real_price - 3.5 * params.atr,
+                round(params.real_price - 2.5 * params.atr, precision),
+                round(params.real_price - 3.5 * params.atr, precision),
             ),  # Stop-loss
             (
-                entry_1 := params.real_price - 0.25 * params.atr,
-                entry_2 := params.real_price - params.atr,
+                round(entry_1, precision),
+                round(entry_2, precision),
             ),  # Entry
             (
-                entry_1 + 0.3 * params.atr,
-                entry_1 + params.atr,
-                entry_1 + 5 * params.atr,
-                entry_2 + 0.3 * params.atr,
-                entry_2 + params.atr,
-                entry_2 + 5 * params.atr,
+                round(entry_1 + 0.3 * params.atr, precision),
+                round(entry_1 + params.atr, precision),
+                round(entry_1 + 5 * params.atr, precision),
+                round(entry_2 + 0.3 * params.atr, precision),
+                round(entry_2 + params.atr, precision),
+                round(entry_2 + 5 * params.atr, precision),
             ),  # Take-profit
         ]
 
     @staticmethod
-    def _calculate_limit_short(params: TradeParams) -> List[Tuple[float, ...]]:
+    def _calculate_limit_short(params: TradeParams, precision: int) -> List[Tuple[float, ...]]:
+        entry_1 = params.real_price + 0.25 * params.atr
+        entry_2 = params.real_price + params.atr
         return [
             (
-                params.real_price + 2.5 * params.atr,
-                params.real_price + 3.5 * params.atr,
+                round(params.real_price + 2.5 * params.atr, precision),
+                round(params.real_price + 3.5 * params.atr, precision),
             ),  # Stop-loss
             (
-                entry_1 := params.real_price + 0.25 * params.atr,
-                entry_2 := params.real_price + params.atr,
+                round(entry_1, precision),
+                round(entry_2, precision),
             ),  # Entry
             (
-                entry_1 - 0.3 * params.atr,
-                entry_1 - params.atr,
-                entry_1 - 5 * params.atr,
-                entry_2 - 0.3 * params.atr,
-                entry_2 - params.atr,
-                entry_2 - 5 * params.atr,
+                round(entry_1 - 0.3 * params.atr, precision),
+                round(entry_1 - params.atr, precision),
+                round(entry_1 - 5 * params.atr, precision),
+                round(entry_2 - 0.3 * params.atr, precision),
+                round(entry_2 - params.atr, precision),
+                round(entry_2 - 5 * params.atr, precision),
             ),  # Take-profit
         ]
