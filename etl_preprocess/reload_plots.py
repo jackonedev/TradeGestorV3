e075d20955db96df404d60a8607b2f675c13bc2f
jackonedev <jackone.action.software@gmail.com>
2024-08-27 16:54:10 -0300

    added: etl_preprocess/reload_plots.py to be executed after parameter optimization

---
+++ etl_preprocess/reload_plots.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+from tools.final_plots import load_3r_plots
+from tools.technical_indicators import squeeze_momentum_indicator
+from utils.utils import (
+    obtain_most_recent_download_directory_paths,
+    obtain_most_recent_downloaded_datasets,
+)
+
+## INITIAL VARIABLES ##
+download_paths = obtain_most_recent_download_directory_paths()
+downloaded_dfs = obtain_most_recent_downloaded_datasets()
+
+
+def reload_plots():
+    """
+    Recarga los gráficos de los activos y temporalidades más recientes
+    utilizando parámetros óptimos para el indicador Squeeze Momentum.
+    """
+
+    download_paths = obtain_most_recent_download_directory_paths()
+    downloaded_dfs = obtain_most_recent_downloaded_datasets()
+
+    for activo, temporalidad_dict in download_paths.items():
+        for temporalidad, path in temporalidad_dict.items():
+
+            df = downloaded_dfs[activo][temporalidad]
+            best_params_file = [f for f in os.listdir(path) if "best_params" in f][0]
+            with open(os.path.join(path, best_params_file)) as f:
+                best_params = json.load(f)
+            params = best_params["best_params"]
+            df = squeeze_momentum_indicator(df, **params)
+            load_3r_plots(df, activo, temporalidad, path)
